651b31d498c45f0849627301bd0161f237991b96
MinhDuc0510 <toladuc510@gmail.com>
2024-03-02 15:18:10 +0700

    Fix error 3/2/2024

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const btnMenu = document.querySelector('.header-menu')
+const overflow = document.querySelector('.overflow')
+const menuBottom = document.querySelector('.header-bottom')
+const deleteBtn = document.querySelector('.header-bottom .icon')
+
+function hideMenuMobile() {
+    overflow.classList.remove('show')
+    overflow.classList.add('hidden')
+
+    menuBottom.classList.remove('show')
+    menuBottom.classList.add('hidden')
+}
+
+btnMenu.addEventListener('click', function () {
+    overflow.classList.remove('hidden')
+    overflow.classList.add('show')
+
+    menuBottom.classList.remove('hidden')
+    menuBottom.classList.add('show')
+
+})
+
+overflow.addEventListener('click', hideMenuMobile)
+
+deleteBtn.addEventListener('click', hideMenuMobile)
